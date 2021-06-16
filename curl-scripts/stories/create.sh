1d6e6a4b31f79f5de4e0fdf5fa3c8d67995a265d
ryansalandy <ryanmsalandy@icloud.com>
2021-06-16 18:33:12 -0400

    Updated Curl Scripts

--- curl-scripts/stories/create.sh
+++ curl-scripts/stories/create.sh
@@ -10,8 +10,7 @@ curl "${API}${URL_PATH}" \
   --data '{
     "story": {
       "hypothesis": "'"${HYPOTHESIS}"'",
-      "tactic": "'"${TACTIC}"'",
-      "votes": "'"${VOTES}"'"
+      "tactic": "'"${TACTIC}"'"
     }
   }'
 
